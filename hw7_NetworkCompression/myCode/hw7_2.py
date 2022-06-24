d60400340fae1a3378fcbbb79e4a60ec940ad6e7
promise <1286232441@qq.COM>
2022-06-24 23:21:10 +0800

    添加可持续训练的脚本hw_7_2_continue

---
+++ hw7_NetworkCompression/myCode/hw7_2.py
@@ -0,0 +1,286 @@
+import torch
+import os
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.models as models
+import re
+import torch
+from glob import glob
+from PIL import Image
+import torchvision.transforms as transforms
+
+
+class StudentNet(nn.Module):
+    '''
+      在這個Net裡面，我們會使用Depthwise & Pointwise Convolution Layer來疊model。
+      你會發現，將原本的Convolution Layer換成Dw & Pw後，Accuracy通常不會降很多。
+
+      另外，取名為StudentNet是因為這個Model等會要做Knowledge Distillation。
+    '''
+
+    def __init__(self, base=16, width_mult=1):
+        '''
+          Args:
+            base: 這個model一開始的ch數量(通道数量)，每過一層都會*2，直到base*16為止。
+            width_mult: 為了之後的Network Pruning使用，在base*8 chs的Layer上會 * width_mult代表剪枝後的ch數量。
+        '''
+        super(StudentNet, self).__init__()
+        multiplier = [1, 2, 4, 8, 16, 16, 16, 16]
+
+        # bandwidth: 每一層Layer所使用的ch數量
+        bandwidth = [ base * m for m in multiplier]
+
+        # 我們只Pruning第三層以後的Layer
+        for i in range(3, 7):
+            bandwidth[i] = int(bandwidth[i] * width_mult)
+
+        self.cnn = nn.Sequential(
+            # 第一層我們通常不會拆解Convolution Layer。
+            nn.Sequential(
+                nn.Conv2d(3, bandwidth[0], 3, 1, 1),
+                nn.BatchNorm2d(bandwidth[0]),
+                nn.ReLU6(),
+                nn.MaxPool2d(2, 2, 0),
+            ),
+            # 接下來每一個Sequential Block都一樣，所以我們只講一個Block
+            nn.Sequential(
+                # Depthwise Convolution
+                nn.Conv2d(bandwidth[0], bandwidth[0], 3, 1, 1, groups=bandwidth[0]),
+                # Batch Normalization
+                nn.BatchNorm2d(bandwidth[0]),
+                # ReLU6 是限制Neuron最小只會到0，最大只會到6。 MobileNet系列都是使用ReLU6。
+                # 使用ReLU6的原因是因為如果數字太大，會不好壓到float16 / or further qunatization，因此才給個限制。
+                nn.ReLU6(),
+                # Pointwise Convolution
+                nn.Conv2d(bandwidth[0], bandwidth[1], 1),
+                # 過完Pointwise Convolution不需要再做ReLU，經驗上Pointwise + ReLU效果都會變差。
+                nn.MaxPool2d(2, 2, 0),
+                # 每過完一個Block就Down Sampling
+            ),
+
+            nn.Sequential(
+                nn.Conv2d(bandwidth[1], bandwidth[1], 3, 1, 1, groups=bandwidth[1]),
+                nn.BatchNorm2d(bandwidth[1]),
+                nn.ReLU6(),
+                nn.Conv2d(bandwidth[1], bandwidth[2], 1),
+                nn.MaxPool2d(2, 2, 0),
+            ),
+
+            nn.Sequential(
+                nn.Conv2d(bandwidth[2], bandwidth[2], 3, 1, 1, groups=bandwidth[2]),
+                nn.BatchNorm2d(bandwidth[2]),
+                nn.ReLU6(),
+                nn.Conv2d(bandwidth[2], bandwidth[3], 1),
+                nn.MaxPool2d(2, 2, 0),
+            ),
+
+            # 到這邊為止因為圖片已經被Down Sample很多次了，所以就不做MaxPool
+            nn.Sequential(
+                nn.Conv2d(bandwidth[3], bandwidth[3], 3, 1, 1, groups=bandwidth[3]),
+                nn.BatchNorm2d(bandwidth[3]),
+                nn.ReLU6(),
+                nn.Conv2d(bandwidth[3], bandwidth[4], 1),
+            ),
+
+            nn.Sequential(
+                nn.Conv2d(bandwidth[4], bandwidth[4], 3, 1, 1, groups=bandwidth[4]),
+                nn.BatchNorm2d(bandwidth[4]),
+                nn.ReLU6(),
+                nn.Conv2d(bandwidth[4], bandwidth[5], 1),
+            ),
+
+            nn.Sequential(
+                nn.Conv2d(bandwidth[5], bandwidth[5], 3, 1, 1, groups=bandwidth[5]),
+                nn.BatchNorm2d(bandwidth[5]),
+                nn.ReLU6(),
+                nn.Conv2d(bandwidth[5], bandwidth[6], 1),
+            ),
+
+            nn.Sequential(
+                nn.Conv2d(bandwidth[6], bandwidth[6], 3, 1, 1, groups=bandwidth[6]),
+                nn.BatchNorm2d(bandwidth[6]),
+                nn.ReLU6(),
+                nn.Conv2d(bandwidth[6], bandwidth[7], 1),
+            ),
+
+            # 這邊我們採用Global Average Pooling。
+            # 如果輸入圖片大小不一樣的話，就會因為Global Average Pooling壓成一樣的形狀，這樣子接下來做FC就不會對不起來。
+            nn.AdaptiveAvgPool2d((1, 1)),
+        )
+        self.fc = nn.Sequential(
+            # 這邊我們直接Project到11維輸出答案。
+            nn.Linear(bandwidth[7], 11),
+        )
+
+    def forward(self, x):
+        out = self.cnn(x)
+        out = out.view(out.size()[0], -1)
+        return self.fc(out)
+
+
+
+def network_slimming(old_model, new_model):
+    params = old_model.state_dict()
+    new_params = new_model.state_dict()
+
+    # selected_idx: 每一層所選擇的neuron index
+    selected_idx = []
+    # 我們總共有7層CNN，因此逐一抓取選擇的neuron index們。
+    for i in range(8):
+        # 根據上表，我們要抓的gamma係數在cnn.{i}.1.weight內。
+        importance = params[f'cnn.{i}.1.weight']
+        # 抓取總共要篩選幾個neuron。
+        old_dim = len(importance)
+        new_dim = len(new_params[f'cnn.{i}.1.weight'])
+        # 以Ranking做Index排序，較大的會在前面(descending=True)。
+        ranking = torch.argsort(importance, descending=True)
+        # 把篩選結果放入selected_idx中。
+        selected_idx.append(ranking[:new_dim])
+
+    now_processed = 1
+    for (name, p1), (name2, p2) in zip(params.items(), new_params.items()):
+        # 如果是cnn層，則移植參數。
+        # 如果是FC層，或是該參數只有一個數字(例如batchnorm的tracenum等等資訊)，那麼就直接複製。
+        if name.startswith('cnn') and p1.size() != torch.Size([]) and now_processed != len(selected_idx):
+            # 當處理到Pointwise的weight時，讓now_processed+1，表示該層的移植已經完成。
+            if name.startswith(f'cnn.{now_processed}.3'):
+                now_processed += 1
+
+            # 如果是pointwise，weight會被上一層的pruning和下一層的pruning所影響，因此需要特判。
+            if name.endswith('3.weight'):
+                # 如果是最後一層cnn，則輸出的neuron不需要prune掉。
+                if len(selected_idx) == now_processed:
+                    new_params[name] = p1[:, selected_idx[now_processed - 1]]
+                # 反之，就依照上層和下層所選擇的index進行移植。
+                # 這裡需要注意的是Conv2d(x,y,1)的weight shape是(y,x,1,1)，順序是反的。
+                else:
+                    new_params[name] = p1[selected_idx[now_processed]][:, selected_idx[now_processed - 1]]
+            else:
+                new_params[name] = p1[selected_idx[now_processed]]
+        else:
+            new_params[name] = p1
+
+    # 讓新model load進被我們篩選過的parameters，並回傳new_model。
+    new_model.load_state_dict(new_params)
+    return new_model
+
+
+class MyDataset(torch.utils.data.Dataset):
+
+    def __init__(self, folderName, transform=None):
+        self.transform = transform
+        self.data = []
+        self.label = []
+
+        for img_path in sorted(glob(folderName + '/*.jpg')):
+            try:
+                # Get classIdx by parsing image path
+                class_idx = int(re.findall(re.compile(r'\d+'), img_path)[1])
+            except:
+                # if inference mode (there's no answer), class_idx default 0
+                class_idx = 0
+
+            image = Image.open(img_path)
+            # Get File Descriptor
+            image_fp = image.fp
+            image.load()
+            # Close File Descriptor (or it'll reach OPEN_MAX)
+            image_fp.close()
+
+            self.data.append(image)
+            self.label.append(class_idx)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        image = self.data[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, self.label[idx]
+
+
+
+trainTransform = transforms.Compose([
+    transforms.RandomCrop(256, pad_if_needed=True, padding_mode='symmetric'),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(15),
+    transforms.ToTensor(),
+])
+testTransform = transforms.Compose([
+    transforms.CenterCrop(256),
+    transforms.ToTensor(),
+])
+
+
+def get_dataloader(mode='training', batch_size=32):
+    assert mode in ['training', 'testing', 'validation']
+    dataset = MyDataset(
+        '/root/autodl-tmp/dataset/food-11/' + mode + "/",
+        transform=trainTransform if mode == 'training' else testTransform)
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=False)
+
+    return dataloader
+
+
+def run_epoch(dataloader, update=True, alpha=0.5):
+    total_num, total_hit, total_loss = 0, 0, 0
+    for now_step, batch_data in enumerate(dataloader):
+        # 清空 optimizer
+        optimizer.zero_grad()
+        # 處理 input
+        inputs, labels = batch_data
+        inputs = inputs.cuda()
+        labels = labels.cuda()
+
+        logits = net(inputs)
+        loss = criterion(logits, labels)
+        if update:
+            loss.backward()
+            optimizer.step()
+
+        total_hit += torch.sum(torch.argmax(logits, dim=1) == labels).item()
+        total_num += len(inputs)
+        total_loss += loss.item() * len(inputs)
+    return total_loss / total_num, total_hit / total_num
+
+
+if __name__ == "__main__":
+    # get dataloader
+    train_dataloader = get_dataloader('training', batch_size=32)
+    valid_dataloader = get_dataloader('validation', batch_size=32)
+
+    net = StudentNet().cuda()
+    net.load_state_dict(torch.load('/root/autodl-tmp/modules/student_custom_small.bin'))
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(net.parameters(), lr=1e-3)
+
+    now_width_mult = 1
+    for i in range(5):
+        now_width_mult *= 0.95
+        new_net = StudentNet(width_mult=now_width_mult).cuda()
+        params = net.state_dict()
+        net = network_slimming(net, new_net)
+        now_best_acc = 0
+        for epoch in range(5):
+            net.train()
+            train_loss, train_acc = run_epoch(train_dataloader, update=True)
+            net.eval()
+            valid_loss, valid_acc = run_epoch(valid_dataloader, update=False)
+            # 在每個width_mult的情況下，存下最好的model。
+            if valid_acc > now_best_acc:
+                now_best_acc = valid_acc
+                torch.save(net.state_dict(), f'custom_small_rate_{now_width_mult}.bin')
+            print('rate {:6.4f} epoch {:>3d}: train loss: {:6.4f}, acc {:6.4f} valid loss: {:6.4f}, acc {:6.4f}'.format(
+                now_width_mult,
+                epoch, train_loss, train_acc, valid_loss, valid_acc))
+
+
